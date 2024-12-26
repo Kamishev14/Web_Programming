03d1111bacd08feba192db9b262b0d21c90b2ee0
Kristijan Kamishev <149070627+Kamishev14@users.noreply.github.com>
2024-12-27 00:13:27 +0100

    Add files via upload

---
+++ VP_LAB(B)/src/main/java/mk/ukim/finki/wp/lab/web/controller/ArtistController.java
@@ -0,0 +1,46 @@
+package mk.ukim.finki.wp.lab.web.controller;
+
+
+import mk.ukim.finki.wp.lab.model.Artist;
+import mk.ukim.finki.wp.lab.service.ArtistService;
+import mk.ukim.finki.wp.lab.service.SongService;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@PreAuthorize("hasAnyRole('ADMIN', 'USER')")
+public class ArtistController {
+    private final ArtistService artistService;
+    private final SongService songService;
+
+    public ArtistController(ArtistService artistService, SongService songService) {
+        this.artistService = artistService;
+        this.songService = songService;
+    }
+
+    @GetMapping("/artist")
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
+    public String getArtists(Model model) {
+        List<Artist> listedArtists = artistService.listArtists();
+        model.addAttribute("listedArtists", listedArtists);
+        return "artistList";
+    }
+
+    @PostMapping("/artist")
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
+    public String postArtist(@RequestParam(required = false) String trackId,
+                             Map<String, Object> model) {
+        List<Artist> listedArtists = artistService.listArtists();
+        model.put("trackId", trackId);
+        model.put("listedArtists", listedArtists);
+        return "artistList"; // Refers to the "artistList.html" template
+    }}
